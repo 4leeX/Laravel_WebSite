331549e0e50af55172c815e4cfac2da18dc7c9d6
Alex <alexsandro.sousa12366@gmail.com>
2020-12-17 20:20:00 -0300

    first commit

---
+++ resources/views/inc/sidebar.blade.php
@@ -0,0 +1,4 @@
+    <div class="sidebar">
+        <h3>Side bar</h3>
+        <p>This is the sidebar</p>
+    </div>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Basic Web Site</title>
+<!-- 	<link rel="stylesheet" href=""> -->
+</head>
+<body>
+	@yield('content')
+
+	@include('inc.sidebar')	
+</body>
+</html>
